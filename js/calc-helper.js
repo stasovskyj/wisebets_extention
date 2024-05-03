97c4e3d08841292c14b7b25c1aec88494552330e
Vitaly Stasovsky <stasovskyj@ukr.net>
2024-05-03 10:18:13 +0300

    Зібрав селектори з основних сайтів Перші спроби відслідковування змін Видалив калькулятор з UI

--- js/calc-helper.js
+++ js/calc-helper.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let mode = 1;
 let calc = document.createElement('div');
 
@@ -174,6 +175,51 @@ function getCurrentSite() {
         }
     }
     return null;
+=======
+function setupBetslipTracking() {
+    const betslipConfig = setupSiteConfig();
+    //console.log(betslipConfig)
+  
+// Створюємо MutationObserver для спостереження за змінами контенту betslip
+    const observer = new MutationObserver((mutationsList) => {
+        for (const mutation of mutationsList) {
+           console.log(mutation);
+            if (mutation.type === 'childList' && mutation.target.matches(betslipConfig.betSlipElement)) {
+                // Викликаємо функцію обробки при зміні контенту betslip
+               
+                processBetslipContent(mutation.target, betslipConfig);
+            } 
+          
+        }
+    });
+
+    // Запускаємо спостереження за змінами тільки в елементі betslip
+    observer.observe(betslipConfig.rootElement, { childList: true, subtree: true, attributes: true, attributeFilter:[], characterData: true });
+}
+
+// Функція для обробки контенту betslip
+function processBetslipContent(betslipNode, betslipConfig) {
+    const amountInput = betslipNode.querySelector(betslipConfig.amountInputElement);
+    const amount = amountInput ? amountInput.value : 'Невідомо';
+
+    const oddsElement = betslipNode.querySelector(betslipConfig.oddsElement);
+    const odds = oddsElement ? oddsElement.textContent : 'Невідомо';
+
+    console.log('Amount:', amount);
+    console.log('Odds:', odds);
+}
+
+// Встановлюємо відслідковування елементів для кожного сайту
+function getCurrentSite() {
+    const hostname = window.location.hostname;
+    let cleanURL = hostname.replace('www.', '');
+    let parts = cleanURL.split('.');
+    if (parts.length == 3) {
+        return parts[1];
+    } else {
+        return parts[0];
+    }
+>>>>>>> 43f80e6 (Зібрав селектори з основних сайтів)
 }
 
 function setupSiteConfig() {
@@ -186,8 +232,12 @@ function setupSiteConfig() {
         return false;
     }
 }
+<<<<<<< HEAD
 function fixValue(v) {
     return (!isNaN(v)) ? (Number.isInteger(v) ? parseInt(v) : parseFloat(v)) : false;
 }
 setupBetslipTracking()
 let a = new Calculator();
+=======
+setupBetslipTracking()
+>>>>>>> 43f80e6 (Зібрав селектори з основних сайтів)
